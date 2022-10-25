1d0294583354ca7f7f27e7b144eabd0916ba6d23
uiuqM <wilque.coelho@mail.uft.edu.br>
2022-10-24 22:26:55 -0300

    atualizando

--- barberShop/src/view/login.java
+++ barberShop/src/view/login.java
@@ -8,10 +8,15 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
 
 public class login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField campoUusuario;
+	private JTextField campoSenha;
 
 	/**
 	 * Launch the application.
@@ -41,11 +46,44 @@ public class login extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		campoSenha = new JTextField();
+		campoSenha.setColumns(10);
+		campoSenha.setBounds(206, 353, 192, 34);
+		contentPane.add(campoSenha);
+		
+		campoUusuario = new JTextField();
+		campoUusuario.setBounds(206, 249, 192, 34);
+		contentPane.add(campoUusuario);
+		campoUusuario.setColumns(10);
+		
+		JLabel labelSenha = new JLabel("Senha");
+		labelSenha.setForeground(Color.WHITE);
+		labelSenha.setFont(new Font("Monospaced", Font.BOLD, 38));
+		labelSenha.setBounds(226, 271, 202, 97);
+		contentPane.add(labelSenha);
+		
+		JLabel labelLogin = new JLabel("Login");
+		labelLogin.setForeground(Color.WHITE);
+		labelLogin.setFont(new Font("Monospaced", Font.BOLD, 38));
+		labelLogin.setBounds(244, 102, 143, 97);
+		contentPane.add(labelLogin);
+		
+		JLabel labelUsuario = new JLabel("Usuario");
+		labelUsuario.setForeground(Color.WHITE);
+		labelUsuario.setFont(new Font("Monospaced", Font.BOLD, 38));
+		labelUsuario.setBounds(216, 163, 202, 97);
+		contentPane.add(labelUsuario);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setIcon(new ImageIcon("D:\\downloads\\output-onlinepngtools.png"));
+		lblNewLabel_1.setBounds(111, 127, 379, 343);
+		contentPane.add(lblNewLabel_1);
+		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(-74, 0, 720, 636);
+		lblNewLabel.setBounds(-64, 0, 720, 636);
 		lblNewLabel.setIcon(new ImageIcon("D:\\projetoBarbearia\\logo.jpg"));
 		contentPane.add(lblNewLabel);
 	}
-
 }
